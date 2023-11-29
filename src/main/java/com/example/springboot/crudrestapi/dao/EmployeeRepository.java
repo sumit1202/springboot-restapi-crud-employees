25ce2013a5da0c6199068f30801389dda27fbdb6
sumitkumar <sumitkumar1202@gmail.com>
2023-11-29 18:34:19 +0530

    commit2

--- src/main/java/com/example/springboot/crudrestapi/dao/EmployeeRepository.java
+++ src/main/java/com/example/springboot/crudrestapi/dao/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package com.example.springboot.crudrestapi.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.springboot.crudrestapi.entity.Employee;
 
+// annotation used incase want to customize path name as "/members" 
+// else "/employees" will be auto generated
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    
+
 }
